ca3bf97706bfc6bdbb08ee08804056d1274ad268
sudikrt <sudarshankaranth18@gmail.com>
2021-02-23 23:29:58 +0530

    throttling

---
+++ throttling.js
@@ -0,0 +1,16 @@
+/**
+ * throttling both are conecpts are usign optiziming concept of web app
+ * limit the rate of function 
+ * 
+ * Lets take a search bar 
+ * suppose we are searching it gives us certain suggessitions
+ * on keypress getresult api call is made.
+ * 
+ * make only api call if time diff b/w two key press event is > then only make 
+ * 
+ * throttling is 
+ *  - if there are alot of events happening 
+ *  - only make the function after certain amount of time
+ *  - like diff b/w last funct call and current func call then only make ai call
+ *  
+ */
