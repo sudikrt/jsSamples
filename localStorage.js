2f04d4cf1d044fbce0dec4e675925788cdfeba6b
sudikrt <sudarshankaranth18@gmail.com>
2021-02-25 23:54:44 +0530

    local storage example

---
+++ localStorage.js
@@ -0,0 +1,14 @@
+//localStorage
+// session storage data is not sent 
+//local storage has highest storage than session and cookies
+//they follow same origin policy 
+//3 things defines the origin
+//1. Protocol
+//2. host 
+//3. port
+
+//localStorage is storage is on windowobject 
+localStorage.setItem ("Hello","world");
+localStorage.getItem ("Hello");
+localStorage.removeItem ('HelloOne');
+localStorage.clear (); // clears everything
